068818850fa76ad72819f157e7d1d2b9e95d3888
desniland <albertdesmond26@gmail.com>
2024-01-02 17:46:35 +0300

    file6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 8):
-    for x in range(i + 1, 10):
-        print("{:d}{:d}".format(i, x), end=', ')
-        print("{:d}{:d}".format(i + 1, x))
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+        print("{:d}{:d}".format(i + 1, j))
