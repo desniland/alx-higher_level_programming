588daac7dfa90c4860f65b6cdee97c2e948a994e
desniland <albertdesmond26@gmail.com>
2024-01-23 16:15:41 +0300

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 
+"""Define a class Square."""
+
+
 class Square:
+    """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
+        Args:
+        size (int): The size of the new square.
+        position (int, int): The position of the new square.
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """sets the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -19,6 +30,7 @@ class Square:
 
     @property
     def position(self):
+        """Gets the current position of the square."""
         return (self.__position)
 
     @position.setter
@@ -42,4 +54,4 @@ class Square:
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
-            print("")
+            print(""i)
