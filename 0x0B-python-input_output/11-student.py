2e26617829c2e158489a8e3e47d3fb6bf55ce14e
desniland <albertdesmond26@gmail.com>
2024-02-06 14:46:22 +0300

    a class Student that defines a student by: (based on 10-student.py)

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -27,7 +27,7 @@ class Student:
         attrs (list): (Optional) attributes to represent.
         """
         if isinstance(attrs, list) \
-                    and all(isinstance(ele, str) for ele in attrs):
+                and all(isinstance(ele, str) for ele in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
