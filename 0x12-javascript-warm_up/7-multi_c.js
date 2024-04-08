f8cfa62e55580246e109be578b033afb12e2900a
desniland <albertdesmond26@gmail.com>
2024-04-08 16:01:34 +0300

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,7 +6,7 @@ if (isNaN(process.argv[2] || process.argv[2] === undefined)) {
   const x = Number(process.argv[2]);
   let i = 0;
   while (i < x) {
-  console.log('C is fun');
-  i++;
+    console.log('C is fun');
+    i++;
 }
 }
