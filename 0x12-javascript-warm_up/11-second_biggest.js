612d5503a9a104f152c4a488412f72ade99b5901
desniland <albertdesmond26@gmail.com>
2024-04-08 14:20:56 +0300

    second Biggest Number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+//Checks for the second biggest number
+if (process.argv.length <= 3) {
+    console.log('0');
+} else {
+    const arguments = process.argv.slice(2).map(Number);
+    const second = arguments.sort(function (a, b) { return b - a; })[1];
+    console.log(second);
+}
