e47f4f2a47355e9e65625ba243e115808de217d0
desniland <albertdesmond26@gmail.com>
2024-04-08 16:08:47 +0300

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-//Checks for the second biggest number
 if (process.argv.length <= 3) {
-    console.log('0');
+  console.log('0');
 } else {
-    const arguments = process.argv.slice(2).map(Number);
-    const second = arguments.sort(function (a, b) { return b - a; })[1];
-    console.log(second);
+  const arguments = process.argv.slice(2).map(Number);
+  const second = arguments.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
