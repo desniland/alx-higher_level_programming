7d18946e1b3a8646dd63f34ed81a97942352e22f
desniland <albertdesmond26@gmail.com>
2024-04-08 17:57:01 +0300

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,11 @@
 
 function factorial (n) {
   if (n < 0) {
-  return (-1);
-}
+    return (-1);
+  }
   if (n === 0 || isNaN(n)) {
-  return (1);
-}
+    return (1);
+  }
   return (n * factorial(n - 1));
 }
 
