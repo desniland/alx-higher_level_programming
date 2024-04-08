ffb986e65a76dec84114c785ec56d3e78b6c1194
desniland <albertdesmond26@gmail.com>
2024-04-08 15:58:18 +0300

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-//prints x times “C is fun”
 if (isNaN(process.argv[2] || process.argv[2] === undefined)) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-    const x = Number(process.argv[2]);
-    let i = 0;
-    while (i < x) {
-        console.log('C is fun');
-        i++;
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
     }
 }
