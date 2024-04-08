092a7c857055458a6ada99f04a871ebef437db6a
desniland <albertdesmond26@gmail.com>
2024-04-08 17:54:02 +0300

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,8 +5,8 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
 } else {
   const x = Number(process.argv[2]);
   let i = 0;
-    while (i < x) {
-      console.log('X'.repeat(x));
-      i++;
-}
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
+  }
 }
