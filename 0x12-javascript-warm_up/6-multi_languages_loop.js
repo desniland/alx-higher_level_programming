87b5dc75ca06beae50af676222f5fa21c849fc3c
desniland <albertdesmond26@gmail.com>
2024-04-08 15:56:44 +0300

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-//looping through array of string in JavaSript
 for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-    console.log(line);
+  console.log(line);
 }
